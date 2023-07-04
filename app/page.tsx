e10158757bffc4639d7c0ffecfd0b03df7ed042d
ahmadmmi <ahmadmmi1904@gmail.com>
2023-07-04 23:00:29 +0700

    base landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,93 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <div>
+      <section className='w-full container mt-[200px]'>
+        <p className='text-[50px] font-bold'>Portofolio</p>
+        <p>
+          I have worker with a couple of innovative brands over years, here is a
+          selection of some my favorite work
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className='grid grid-cols-5 items-center gap-[40px] mt-[40px]'>
+          <Image
+            src='/image/icons8-tokopedia.svg'
+            alt='tokopedia'
+            width='100'
+            height='100'
+          />
+          <Image
+            src='/image/icons8-shopee.svg'
+            alt='shopee'
+            width='100'
+            height='100'
+          />
+          <Image
+            src='/image/icons8-lazada.svg'
+            alt='lazada'
+            width='100'
+            height='100'
+          />
         </div>
-      </div>
+        <div className='inline-flex mt-[80px] space-x-[24px] text-[20px]'>
+          <span className='text-neutral-900 font-bold'> All Works</span>
+          <span> Andorid Development</span>
+          <span> Web Development</span>
+          <span> UI & UX</span>
+          <span> Game Development</span>
+        </div>
+        <div className='grid grid-cols-3 gap-[40px] mt-[20px]'>
+          <div className='aspect-square bg-zinc-200 rounded-xl'></div>
+          <div className='aspect-square bg-zinc-200 rounded-xl'></div>
+          <div className='aspect-square bg-zinc-200 rounded-xl'></div>
+          <div className='aspect-square bg-zinc-200 rounded-xl'></div>
+          <div className='aspect-square bg-zinc-200 rounded-xl'></div>
+          <div className='aspect-square bg-zinc-200 rounded-xl'></div>
+          <div className='aspect-square bg-zinc-200 rounded-xl'></div>
+          <div className='aspect-square bg-zinc-200 rounded-xl'></div>
+          <div className='aspect-square bg-zinc-200 rounded-xl'></div>
+        </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className='border border-neutral-500 w-1/3 container my-[200px]'></div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section className='flex flex-row w-full container'>
+        <div className='w-1/2'>
+          <Image
+            src='/image/genshinHomeSmall.jpg'
+            alt='image1'
+            width='720'
+            height='720'
+            className='w-4/6 border-4 border-white rounded-xl'
+          />
+        </div>
+        <div className='w-1/2'>
+          <div className='flex flex-col w-1/2'>
+            <p className='text-[50px] font-bold'>Genshin</p>
+            <p>Explore The World</p>
+            <div className='inline-flex mt-[100px] space-x-[24px]'>
+              <button className='text-[24px] py-[12px] px-[40px] bg-black rounded-full text-white hover:shadow-lg'>
+                Play
+              </button>
+              <button className='text-[24px] py-[12px] px-[40px] border-2 border-black rounded-full hover:shadow-lg'>
+                Guide
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <div className='border border-neutral-500 w-1/3 container my-[200px]'></div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <section className='text-center w-full container'>
+        <p className='text-[50px] font-bold'>Genshin</p>
+        <p className='text-[24px]'>Explore The World</p>
+        <div className='mt-[50px]'>
+          <button className='text-[24px] py-[12px] px-[40px] bg-black rounded-full text-white hover:shadow-lg'>
+            Play
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 }
