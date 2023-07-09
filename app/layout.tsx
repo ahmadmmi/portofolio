31b506d9e86831338075fed3a0cc824451175263
ahmadmmi <ahmadmmi1904@gmail.com>
2023-07-09 13:45:14 +0700

    responsive header

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
-        <section className='w-full bg-[#F6C71F] h-screen'>
+        <section className='w-full bg-[#F6C71F] h-screen '>
           <div className='flex flex-col container h-full'>
             <nav className='flex flex-row justify-between pt-[58px]'>
               <span className='flex flex-row font-bold'>AM</span>
@@ -18,27 +18,26 @@ export default function RootLayout({
                 alt='menu'
                 width='24'
                 height='24'
-                // cursor-pointer='true'
               />
             </nav>
             <div className='flex grow flex-row'>
-              <div className='flex flex-col justify-center w-1/2'>
-                <p className='text-[85px] leading-tight font-medium '>
+              <div className='flex flex-col justify-center'>
+                <p className='text-[42px] md:text-[85px] leading-tight font-medium '>
                   Hello I&apos;m Maulana, <br />
                   I&apos;m Develop Stuff.
                 </p>
-                <p className='text-[24px] mt-[40px] '>
+                <p className='text-xl md:text-2xl mt-10 '>
                   Lets collaborate on your next project.
                 </p>
-                <div className='inline-flex mt-[100px] space-x-[24px]'>
-                  <button className='text-[24px] py-[12px] px-[40px] bg-black rounded-full text-white hover:shadow-lg'>
+                <div className='inline-flex mt-[50px] md:mt-[100px] space-x-[24px]'>
+                  <button className='basis-1/2 text-[12px] md:text-[24px] py-[12px] px-[24px] bg-black rounded-full text-white hover:shadow-lg'>
                     Lets Work
                   </button>
-                  <button className='text-[24px] py-[12px] px-[40px] border-2 border-black rounded-full hover:shadow-lg'>
-                    See my work
+                  <button className='basis-1/2 text-[12px] md:text-[24px] py-[12px] px-[24px] border-2 border-black rounded-full hover:shadow-lg'>
+                    See My Work
                   </button>
                 </div>
-                <div className='inline-flex mt-[100px] space-x-[24px]'>
+                <div className='inline-flex mt-[50px] md:mt-[100px] space-x-[24px]'>
                   <Image
                     src='/image/icons8-facebook.svg'
                     alt='facebook'
